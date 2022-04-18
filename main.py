f13a091b1751bf351d86e1c369fa6267faae7249
andrzmil <andrz.milewski@gmail.com>
2022-04-18 17:14:19 +0200

    fixed the way of appearing components

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter.filedialog import askopenfilename
 from dash.exceptions import PreventUpdate
 from dash import callback_context
 import random
+from numpy import append
 
 import pandas as pd
 
@@ -25,7 +26,9 @@ app.layout = html.Div(children=[
         
     ], style={'width': '100%', "background": "cornflowerblue"}, id='buttons-bar'),
     
-    html.Div(children=[], id='input-div', style={"background": "cornflowerblue"}),
+    html.Div(children=[], id='manage-div', style={"background": "cornflowerblue"}),
+    
+    html.Div(children=[], id='input-div'),
     
     html.Div(children=[], id='outputs-div', style={'display': 'flex'})
     
@@ -39,10 +42,11 @@ app.layout = html.Div(children=[
 
 ###########ON create button click - refresh input div or on create + additionaly on close project
 @app.callback(
-    Output('input-div', 'children'),
+    Output('manage-div', 'children'),
     Input('create-new', 'n_clicks')    
 )
 def on_button_click(button_clicks):
+    global ids
     if button_clicks is None:
         raise PreventUpdate
     else:
@@ -50,41 +54,41 @@ def on_button_click(button_clicks):
         fl = askopenfilename()
         if fl:
             df = pd.read_csv(fl)
+            
             ids = list(set(df['YEAR'])) 
             
             children = [
                 dcc.RadioItems(options= {'TM': 'TO MATCH', 'DF': 'DEFERRED'}, id='status', value="TM"),
-                dcc.Dropdown(options=ids, id='choose-id', value=ids[0]),
                 html.Button(children='MATCH', id='match-button', style={'width': '30%'}),
                 html.Button(children='DEFER', id='defer-button'),
                 html.Button(children='EXPORT', id='export-button')
                     
-                    ]
-            del(fl)
-            return children
-        else:
-            children = []
+            ]
+            
             return children
         
-        
+#######on click to match/deferred        
+@app.callback(
+    Output('input-div', 'children'),
+    Input('status', 'value')
+)
+def on_change_status(value):
+    children = []
+    if value=="TM":
+            children.append(dcc.Dropdown(options=ids[0:2], id='choose-id'))
+    else:
+            children.append(dcc.Dropdown(options=ids[1:4], id='choose-id'))
+    return children
 
 
-
-
-#############callback on change id from dropdownlist   
-############affects both - input list (choose_id_value) and status (to_match/deferred - status_value) 
+###############on change id value
 @app.callback(
     Output('outputs-div', 'children'),
-    [Input('choose-id', 'value'),
-     Input('status', 'value')]    
+    Input('choose-id', 'value')
 )
-def on_change_id(choose_id_value, status_value):
-    #if choose_id_value or status_value is None:
-    #    raise PreventUpdate
-    #else:
-    
-        if choose_id_value:
-            children_id = [
+def on_change_id(choose_id_value):
+    if choose_id_value:
+        children_id = [
             html.Div(children=[
                 html.Label(children='TITLE TO MATCH', id='title-to-match'),
                 dcc.RadioItems(options=[i for i in [random.random() for i in range(50)]], id='selected-title')
@@ -92,16 +96,45 @@ def on_change_id(choose_id_value, status_value):
             
             html.Div(children=[], id='authors-div', style={"background": "yellow", "width": "40%"})
             ]
-        elif status_value:
-            children_id = [
-            html.Div(children=[
-                html.Label(children='TITLE TO MATCH', id='title-to-match'),
-                dcc.RadioItems(options=[i for i in [random.random() for i in range(20)]], id='selected-title')
-                ], style={"width":"20%", "overflow-x":"hidden", "overflow-y":"auto", "height": "600px", "background": "grey"}),
-            
-            html.Div(children=[], id='authors-div', style={"background": "yellow", "width": "40%"})
-            ]
         return children_id
+
+
+
+
+#############callback on change id from dropdownlist   
+############affects both - input list (choose_id_value) and status (to_match/deferred - status_value) 
+# @app.callback(
+#     [Output('choose-id', 'options'),
+#     Output('outputs-div', 'children')],
+#     [Input('choose-id', 'value'),
+#      Input('status', 'value')]    
+# )
+# def on_change_id(choose_id_value, status_value):
+#     #if choose_id_value or status_value is None:
+#     #    raise PreventUpdate
+#     #else:
+    
+#         if choose_id_value:
+#             children_id = [
+#             html.Div(children=[
+#                 html.Label(children='TITLE TO MATCH', id='title-to-match'),
+#                 dcc.RadioItems(options=[i for i in [random.random() for i in range(50)]], id='selected-title')
+#                 ], style={"width":"20%", "overflow-x":"hidden", "overflow-y":"auto", "height": "600px", "background": "grey"}),
+            
+#             html.Div(children=[], id='authors-div', style={"background": "yellow", "width": "40%"})
+#             ]
+            
+#         elif status_value:
+#             children_id = [
+#             html.Div(children=[
+#                 html.Label(children='TITLE TO MATCH', id='title-to-match'),
+#                 dcc.RadioItems(options=[i for i in [random.random() for i in range(20)]], id='selected-title')
+#                 ], style={"width":"20%", "overflow-x":"hidden", "overflow-y":"auto", "height": "600px", "background": "grey"}),
+            
+#             html.Div(children=[], id='authors-div', style={"background": "yellow", "width": "40%"})
+#             ]
+#             options = ids
+#         return children_id
 
 
 
